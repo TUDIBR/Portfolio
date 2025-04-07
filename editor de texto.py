a0cd65f91cba58048b315018a6f5367499d81300
TUDIBR <tudibr555@gmail.com>
2025-04-07 13:29:23 -0300

    Update editor de texto.py

--- editor de texto.py
+++ editor de texto.py
@@ -1,34 +1,46 @@
 from customtkinter import *
 from customtkinter import filedialog
+from CTkMessagebox import CTkMessagebox
 import os
+
 
 app = CTk()
 
+# Funções
 class functions():
-    # Abre o arquivo txt
+    # Abre o arquivo tfrom CTkMessagebox import CTkMessagebox
     def abrir_arquivo(self):
-        self.arquivo = filedialog.askopenfile(title='Selecione um arquivo de texto', filetypes=[('Arquivo de texto', '*.txt')])
-
+        try:
+            self.arquivo = filedialog.askopenfile(title='Selecione um arquivo de texto', filetypes=[('Arquivo de texto', '*.txt')])
+        except FileNotFoundError:
+            pass
         if not self.arquivo == '' or not self.arquivo == None:
-            self.nome_extensao = os.path.basename(self.arquivo.name)
-            self.nome = os.path.splitext(self.nome_extensao)[0]
-            self.tv_editor.add(self.nome)
+            try:
+                self.nome_extensao = os.path.basename(self.arquivo.name)
+                self.nome = os.path.splitext(self.nome_extensao)[0]
+                self.tv_editor.add(self.nome)
             
-            self.tb_editor_text = CTkTextbox(master=self.tv_editor.tab(self.nome), border_spacing=15, font=('consolas', 17, 'bold'))
-            self.tb_editor_text.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor='center')
-            self.tv_editor.set(self.nome)
-            self.tb_editor_text.insert('1.0', self.arquivo.read())
+                self.tb_editor_text = CTkTextbox(master=self.tv_editor.tab(self.nome), border_spacing=15, font=('consolas', 17, 'bold'))
+                self.tb_editor_text.place(relx=0.5, rely=0.5, relwidth=1, relheight=1, anchor='center')
+                self.tv_editor.set(self.nome)
+                self.tb_editor_text.insert('1.0', self.arquivo.read())
+            
+            except Exception:
+                return
 
     # Salva o arquivo txt
     def salvar_arquivo(self):
         self.arquivo = filedialog.asksaveasfilename(title='Salvar como...',
                                                     defaultextension='.txt',
                                                     filetypes=[('Arquivo de texto', '*.txt')])
+        
         if not self.arquivo == '' or not self.arquivo == None:
             self.conteudo = self.tb_editor_text.get('1.0', 'end')
-            print(self.conteudo)
-            with open(self.arquivo) as arquivo_:
+
+            with open(self.arquivo, 'w') as arquivo_:
                 arquivo_.write(self.conteudo)
+                CTkMessagebox(title="Salvamento", message="Arquivo salvo com sucesso", icon="info")
+
 
 # Programa principal
 class main(functions):
@@ -60,6 +72,7 @@ class main(functions):
         # Salvar arquivo
         self.bt_salvar = CTkButton(master=app, text='Salvar arquivo', command=self.salvar_arquivo)
         self.bt_salvar.place(relx=0.33, rely=0.04)
+
 
 if __name__ == "__main__":       
     main()
