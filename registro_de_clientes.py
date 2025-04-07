967501325d90d6fae6d36a2355e10f5fa149f161
TUDIBR <tudibr555@gmail.com>
2025-04-06 22:25:59 -0300

    Update modulos registro_de_clientes.py

--- registro_de_clientes.py
+++ registro_de_clientes.py
@@ -1,4 +1,15 @@
-from modulos import *
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkcalendar import Calendar, DateEntry
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import SimpleDocTemplate
+import webbrowser
+import sqlite3
+import requests
 
 root = Tk()
 
